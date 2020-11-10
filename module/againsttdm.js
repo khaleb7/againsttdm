29130e1db22578ad461f566114bb00fb59231f5f
khaleb7 <josh.rasey@gmail.com!>
2020-11-10 09:09:22 -0600

    Part 2

---
+++ module/againsttdm.js
@@ -0,0 +1,47 @@
+// Import Modules
+import { againstTdmActor } from "./actor/actor.js";
+import { againstTdmActorSheet } from "./actor/actor-sheet.js";
+import { againstTdmItem } from "./item/item.js";
+import { againstTdmItemSheet } from "./item/item-sheet.js";
+
+Hooks.once('init', async function() {
+
+  game.againsttdm = {
+    againstTdmActor,
+    againstTdmItem
+  };
+
+  /**
+   * Set an initiative formula for the system
+   * @type {String}
+   */
+  CONFIG.Combat.initiative = {
+    formula: "1d20",
+    decimals: 2
+  };
+
+  // Define custom Entity classes
+  CONFIG.Actor.entityClass = againstTdmActor;
+  CONFIG.Item.entityClass = againstTdmItem;
+
+  // Register sheet application classes
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("againsttdm", againstTdmActorSheet, { makeDefault: true });
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet("againsttdm", againstTdmItemSheet, { makeDefault: true });
+
+  // If you need to add Handlebars helpers, here are a few useful examples:
+  Handlebars.registerHelper('concat', function() {
+    var outStr = '';
+    for (var arg in arguments) {
+      if (typeof arguments[arg] != 'object') {
+        outStr += arguments[arg];
+      }
+    }
+    return outStr;
+  });
+
+  Handlebars.registerHelper('toLowerCase', function(str) {
+    return str.toLowerCase();
+  });
+});
